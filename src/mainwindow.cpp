ffde681fe10a13ea646061ad667fa49a677d2680
Anirban Dey <code@soufrabi.com>
2024-01-04 22:38:50 +0530

    testing web notifications

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,6 +6,8 @@
 #include <QWebEngineSettings>
 #include <QWebEnginePage>
 #include <QWebEngineDownloadRequest>
+#include <QWebEnginePage>
+#include <QDebug>
 
 
 MainWindow::MainWindow(QWidget *parent)
@@ -14,14 +16,14 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-
     QVBoxLayout *vbox_main = new QVBoxLayout(this);
     ui->centralwidget->setLayout(vbox_main);
     std::string url_whatsapp = "https://web.whatsapp.com";
     std::string url_duckduckgo = "https://duckduckgo.com" ;
     std::string url_test1 = "https://github.com/neovim/neovim/releases" ;
+    std::string url_test_notification = "https://cleverpush.com/en/test-notifications/" ;
 
-    QUrl url = QUrl(QString::fromStdString(url_whatsapp));
+    QUrl url = QUrl(QString::fromStdString(url_test_notification));
 
     const std::string user_agent_firefox_linux115 = "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0";
     QString user_agent = QString::fromStdString(user_agent_firefox_linux115);
@@ -35,12 +37,30 @@ MainWindow::MainWindow(QWidget *parent)
     profile->setHttpUserAgent(user_agent);
     // page using profile
     QWebEnginePage *page = new QWebEnginePage(profile);
+    page->settings()->setAttribute(QWebEngineSettings::XSSAuditingEnabled, false);
+    page->settings()->setUnknownUrlSchemePolicy(QWebEngineSettings::AllowAllUnknownUrlSchemes);
+    QObject::connect(page, &QWebEnginePage::featurePermissionRequested,
+                       [&] (const QUrl &origin, QWebEnginePage::Feature feature) {
+                              qDebug() << "NOtif \n";
+                              qDebug() << feature;
+                              try {
+                           if (feature == QWebEnginePage::Notifications) {
+
+                                page->setFeaturePermission(origin, feature, QWebEnginePage::PermissionGrantedByUser);
+                           }
+                              }catch(const std::exception &e){
+                                    qDebug() << e.what(); // information from length_error printed
+                              }
+                       });
 
 //    QWebEngineProfile::defaultProfile()->setHttpUserAgent(user_agent);
      view->setPage(page);
      view->setUrl(url);
 
+
+
     vbox_main->addWidget(view);
+
 
 }
 
